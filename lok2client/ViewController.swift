38dfb3b35388b9c57926474e0ef1bdd70ebad1fb
bios90 <bios90@mail.ru>
2020-09-22 20:16:51 +0300

    Initial Commit

---
+++ lok2client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lok2client
+//
+//  Created by Филипп Бесядовский on 22.09.2020.
+//  Copyright © 2020 dimfcompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
